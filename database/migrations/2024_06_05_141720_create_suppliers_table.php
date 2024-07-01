d709bb8eaff4ed60740014d2fbdae6e2fbcbe14a
Jose F. D'Silva <jose.dsilva@axelerant.com>
2024-07-01 20:51:41 +0530

    Task 2: created migrations

---
+++ database/migrations/2024_06_05_141720_create_suppliers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('contact_name');
+            $table->text('address');
+            $table->string('city', 100);
+            $table->string('postal_code', 10);
+            $table->string('country', 20);
+            $table->string('phone', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('suppliers');
+    }
+};

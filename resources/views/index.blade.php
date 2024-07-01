972660a22f41b255572f25030b5ddafb598dab61
Jose F. D'Silva <jose.dsilva@axelerant.com>
2024-07-01 16:25:35 +0530

    added basic Bootstrap5 layout for demos

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layout-base')
+
+@section('main_content')
+<h1 class="text-center py-2">Topics</h1>
+<div class="list-group">
+  <a href="#" class="list-group-item list-group-item-action">
+    <div class="d-flex w-100 justify-content-between">
+      <h3 class="mb-1">Usecase Heading</h3>
+    </div>
+    <p class="mb-1">Short description for this usecase</p>
+  </a>
+</div>
+@endsection
